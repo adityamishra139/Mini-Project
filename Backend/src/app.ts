1c60d9713c3b560f0c2d16850ab9d30b9b1ce8f3
Abhishek Agarwal <iamabhishekagarwal@gmail.com>
2024-10-17 01:08:01 +0530

    Added Dockerfile for backend

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 
-const port = 3000;
+
 const app = express();
 
 app.get('/' , (req,res)=>{
     res.send("Hello world");
 })
 
-app.listen(port , ()=>{
-    console.log("app listening to port ",port);
+app.post('/signin' , (req,res)=>{
+})
+
+app.listen(3000 , ()=>{
+    console.log("app listening to port ",3000);
 })
