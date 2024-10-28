c8732534560e8f377a2b1f07c83b3539c5faad46
Abhishek Agarwal <iamabhishekagarwal@gmail.com>
2024-10-28 16:56:21 +0530

    new updates

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,32 +1,80 @@
 import { useEffect, useState } from "react";
 import { authorizeCheck } from "../authorizedCheck";
+import {
+  IconChartLine,
+  IconHome,
+  IconHelp,
+  IconUserCircle,
+  IconBook2,
+} from "@tabler/icons-react";
+import { FloatingDock } from "../components/floatingNavbar";
+
+const links = [
+  {
+    title: "Home",
+    icon: <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+    href: "/",
+  },
+  {
+    title: "Profile",
+    icon: <IconUserCircle className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+    href: "#",
+  },
+  {
+    title: "Quiz",
+    icon: <IconBook2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+    href: "#",
+  },
+  {
+    title: "Performance",
+    icon: <IconChartLine className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+    href: "#",
+  },
+  {
+    title: "Help",
+    icon: <IconHelp className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
+    href: "#",
+  },
+];
 
 function Home() {
   const [authorized, setAuthorized] = useState(false);
-  const [loading, setLoading] = useState(true); // Add loading state
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const checkAuth = async () => {
       const response = await authorizeCheck();
       setAuthorized(response);
-      setLoading(false); 
+      setLoading(false);
     };
     checkAuth();
   }, []);
 
   useEffect(() => {
-    if (!loading && !authorized) { 
-      alert('You are not logged in');
+    if (!loading && !authorized) {
       window.location.href = '/signin'; 
     }
-  }, [authorized, loading]); 
+  }, [authorized, loading]);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-center">Loading...</div>
+      </div>
+    );
   }
 
   return (
-    <div>This is the home page</div>
+    <>
+      <div className="flex flex-col items-center h-screen">
+        <div className="flex-grow flex items-center justify-center">
+          <div className="text-center">This is the home page</div>
+        </div>
+      </div>
+      <div className="fixed bottom-0 md:left-1/2 md:transform md:-translate-x-1/2 left-4 mb-4">
+        <FloatingDock items={links} />
+      </div>
+    </>
   );
 }
 
